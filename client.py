dee642e84fde2c996b9aed2c76a07e87785124b6
degrace1 <gracemary.depietro@verizon.net>
2021-04-05 14:25:37 -0500

    added directions in client.py

--- client.py
+++ client.py
@@ -12,25 +12,24 @@ import sys
 
 port = "5556"
 if len(sys.argv) > 1:
-    port =  sys.argv[1]
+    port = sys.argv[1]
     int(port)
 
 if len(sys.argv) > 2:
-    port1 =  sys.argv[2]
+    port1 = sys.argv[2]
     int(port1)
 
 context = zmq.Context()
-print ("Connecting to server...")
+print("Connecting to server...")
 socket = context.socket(zmq.REQ)
-socket.connect ("tcp://localhost:%s" % port)
+socket.connect("tcp://localhost:%s" % port)
 if len(sys.argv) > 2:
-    socket.connect ("tcp://localhost:%s" % port1)
+    socket.connect("tcp://localhost:%s" % port1)
 
 #  Do 10 requests, waiting each time for a response
-for request in range (1,10): # FIXME change this to be when u need it, like after servos are moving
-    print ("Sending request ", request,"...")
-    socket.send ("Plz give me location")
+for request in range(1, 10):  # FIXME change this to be when u need it, like after servos are moving
+    print("Sending request ", request, "...")
+    socket.send("Plz give me location")
     #  Get the reply.
     message = socket.recv()
-    print ("Received reply ", request, "[", message, "]") # FIXME add splitter for message
-
+    print("Received reply ", request, "[", message, "]")  # FIXME add splitter for message
