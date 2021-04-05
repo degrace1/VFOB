57b50e21e467b001dac2252636d231314873d073
degrace1 <gracemary.depietro@verizon.net>
2021-04-05 14:17:36 -0500

    added directions in client.py

--- client.py
+++ client.py
@@ -1,3 +1,12 @@
+# Made By Grace on 4/2/21
+# Name: client.py
+# This program connects to the server through port "5556"
+# and sends a message asking for info.
+# What needs to be changed:
+# - change request to send only when needed
+# - change message to be split differently (will be sent in the format "Distance=#,x=#,b=#")
+
+
 import zmq
 import sys
 
@@ -18,9 +27,10 @@ if len(sys.argv) > 2:
     socket.connect ("tcp://localhost:%s" % port1)
 
 #  Do 10 requests, waiting each time for a response
-for request in range (1,10): #change this to be when u need it, like after servos are moving
+for request in range (1,10): # FIXME change this to be when u need it, like after servos are moving
     print ("Sending request ", request,"...")
     socket.send ("Plz give me location")
     #  Get the reply.
     message = socket.recv()
-    print ("Received reply ", request, "[", message, "]")
+    print ("Received reply ", request, "[", message, "]") # FIXME add splitter for message
+
